08de04549a65ee0aa1c9498cfbf683846069c1f3
MaxVillagomez <maxvillagomez5141@gmail.com>
2022-06-24 14:31:41 -0500

    currently trying to write javascript. trying to add Xs and Os. Figured out win condition

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
-document.getElementById("first-section").onclick = function () {
-    let name = prompt("Enter your name");
-    document.getElementById("firsttext").innerText = name;
-}
+let playerText = document.getElementById('playerText');
+let restartButton = document.getElementById('resetButton');
+let boxes = Array.from(document.getElementsByClassName('grid-container'));
 
-// document.getElementById("city").onclick = function () {
-//     var city = prompt("Enter your city");
-//     document.getElementById("outputCity").innerText = city;
+const letterX = "X";
+const letterO = "O";
+let currentPlayer = letterX;
+let emptySpaces = Array(9).fill(null);
+
+console.log(emptySpaces)
+// const playerOneName = document.getElementById('firsttext');
+// const playerTwoName = document.getElementById('secondtext');
+// const button1 = document.getElementById('button1');
+// const playerNameOutput = document.getElementById('players-name-input')
+
+// function displayCurrentPlayer() {
+//     playerNameOutput.innerHTML = playerOneName.value + "'s turn";
 // }
 
-// const gameState = {
-//     players: ['x', 'o'],
-//     board: [
-//         [null, null, null],
-//         [null, null, null],
-//         [null, null, null]
-//     ]
-// }
+// button1.addEventListener('click', displayCurrentPlayer)
 
-// // state
-// let initialState;
 
-// function buildInitialState() {
 
-// }
+//ADDING X'S OR O'S
+
+
+// WIN CONDITION
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+
 
 // // render
 // function renderState() {
