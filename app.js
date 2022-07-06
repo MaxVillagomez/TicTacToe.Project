6f9e3658bb4577a6661154908e775301416f2677
MaxVillagomez <maxvillagomez5141@gmail.com>
2022-07-06 14:54:37 -0500

    updated javascript file

--- app.js
+++ app.js
@@ -1,28 +1,74 @@
-let cells = document.querySelectorAll('.cell')
-cells = Array.from(cells)
-const playerOne = "X"
-const playerTwo = "O"
-
-let currentPlayer = "X"
-
-function gameStart() {
-    let emptyBoard = [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""],
-    ]
-
+let gameState = {
+    gameBoard: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ],
+    currentPlayer: 'X',
+    players: ['X', 'O']
 }
 
-const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+// 1) What part of my website do I want to build on? 
+    // Usually deals with document.get_____
+// 2) What do I want to teach that part of my website to do? 
+    // Equivalent of writing a callback function (which we will pass into our event listeners)
+// 3) How do we connect parts 1 and 2 together? 
+    // Equivalent of creating an event listener 
 
+// Problem #1: Creating the gameboard 
+document.addEventListener("DOMContentLoaded", () => { 
+    // 1) I need to grab the part of my HTML where I want to create my game board
+    let appElement = document.getElementById('app'); 
+    createGameBoard();
 
+    // 2) I want to create a function that creates a game board element with 3x3 number of cells 
+    function createGameBoard () {
+        // What do I want to create?
+        // A: I need to first create a HTML row. 
+        // let rowElement = document.createElement('tr');
+
+        // for (let i = 1; i <= 3; i++) {
+        //     let cellElement = document.createElement('td'); 
+        //     rowElement.appendChild(cellElement);
+        // }
+
+        // appElement.appendChild(rowElement); 
+
+        // CODE VERSION OF CONNECTING THIS TO YOUR JS STATE
+        for (let i = 0; i < gameState.gameBoard.length; i++) {
+            let rowElement = document.createElement('tr'); 
+
+            let jsRow = gameState.gameBoard[i]
+            for (let j = 0; j < jsRow.length; j++) {
+                let cellElement = document.createElement('td'); 
+                let currentJSCell = jsRow[j]
+                // let currentJSCell = gameState.gameBoard[i][j]
+
+                cellElement.addEventListener('click', colorInCell); 
+
+                if (currentJSCell === 'X') {
+                    cellElement.innerText = gameState.c
+                } else if (currentJSCell === 'O') {
+                    cellElement.innerText = 'O'
+                }
+                rowElement.appendChild(cellElement);
+            }
+            appElement.appendChild(rowElement); 
+        }
+    }; 
+
+    function colorInCell (evt) {
+        console.log('This is the evt object', evt); 
+        let targetedElement = evt.target;
+        console.log('I am the targeted element: ', targetedElement); 
+        targetedElement.innerText = 'Hi Max!'
+        targetedElement.style.backgroundColor = 'lavender'; 
+    }
+})
+
+// Problem #2: Getting your cells to do SOMETHING
+// Step 1) Grab the part of your website you awnt to do something
+    // I did that in my nested for loop above where I create a td element. 
+// Step 2) What do I wnat to teach the cells to do?
+    // I need to write a callback function 
+// Step 3) How do we connect these two steps together?
