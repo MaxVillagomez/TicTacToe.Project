8808df5c935bc3e7d35f98e3162ff33e05ab2701
MaxVillagomez <maxvillagomez5141@gmail.com>
2022-08-22 10:19:02 -0500

    final push

--- app.js
+++ app.js
@@ -1,99 +1,141 @@
-let gameState = {
-    gameBoard: [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-    ],
-    currentPlayer: 'X',
-    players: ['X', 'O']
+const tiles = Array.from(document.querySelectorAll(".tile"));
+const playerDisplay = document.querySelector("#display-player");
+const resetButton = document.querySelector("#reset");
+const announcer = document.querySelector(".announcer");
+const playerInput = document.querySelectorAll(".name-input");
+const getNames = document.getElementById("getnames");
+
+console.dir(playerInput);
+let board = ["", "", "", "", "", "", "", "", ""];
+let isX = "X";
+let isGameActive = true;
+let indexOfPlayer = getRandomInt(2);
+const playerXWon = "Player X Won";
+const playerOWon = "Player O Won";
+const draw = "Draw";
+let currentPlayer;
+
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+getNames.addEventListener("click", handleNames);
+
+function handleNames() {
+  currentPlayer = indexOfPlayer ? playerInput[1].value : playerInput[0].value;
+  console.log(currentPlayer);
+  playerDisplay.textContent = `${currentPlayer}'s turn`;
+  document.getElementsByClassName('game-header') = '';
 }
 
-// 1) What part of my website do I want to build on? 
-    // Usually deals with document.get_____
-// 2) What do I want to teach that part of my website to do? 
-    // Equivalent of writing a callback function (which we will pass into our event listeners)
-// 3) How do we connect parts 1 and 2 together? 
-    // Equivalent of creating an event listener 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
-// Problem #1: Creating the gameboard 
-document.addEventListener("DOMContentLoaded", () => { 
-    // 1) I need to grab the part of my HTML where I want to create my game board
-    let boardElement = document.getElementById('board'); 
-    createGameBoard();
+playerDisplay.textContent = "Please enter Player 1 and Player 2 names.";
 
-    // 2) I want to create a function that creates a game board element with 3x3 number of cells 
-    function createGameBoard () {
-        // What do I want to create?
-        // A: I need to first create a HTML row. 
-        // let rowElement = document.createElement('tr');
+const isValidAction = (tile) => {
+  if (tile.innerText === "X" || tile.innerText === "O") {
+    return false;
+  }
 
-        // for (let i = 1; i <= 3; i++) {
-        //     let cellElement = document.createElement('td'); 
-        //     rowElement.appendChild(cellElement);
-        // }
+  return true;
+};
 
-        // appElement.appendChild(rowElement); 
+const updateBoard = (index) => {
+  board[index] = isX;
+};
 
-        // CODE VERSION OF CONNECTING THIS TO YOUR JS STATE
-        for (let i = 0; i < gameState.gameBoard.length; i++) {
-            let rowElement = document.createElement('tr'); 
-            let jsRow = gameState.gameBoard[i]
-            for (let j = 0; j < jsRow.length; j++) {
-                let cellElement = document.createElement('td'); 
-                let currentJSCell = jsRow[j]
-                // let currentJSCell = gameState.gameBoard[i][j]
-                // button.setAttribute("name", "helloButton");
-                cellElement.setAttribute('class', 'cell');
-                cellElement.addEventListener('click', addLetter);
-                if (currentJSCell === 'X') {
-                    cellElement.innerText = 'X'
-                } else if (currentJSCell === 'O') {
-                    cellElement.innerText = 'O'
-                }
+const changePlayer = () => {
+  playerDisplay.classList.remove(`player${isX}`);
+  if (isX === "X") {
+    isX = "O";
+    indexOfPlayer = Math.abs(indexOfPlayer - 1);
+    console.log(indexOfPlayer);
+    currentPlayer = playerInput[indexOfPlayer].value;
+  } else {
+    isX = "X";
+    indexOfPlayer = Math.abs(indexOfPlayer - 1);
+    console.log(indexOfPlayer);
+    currentPlayer = playerInput[indexOfPlayer].value;
+  }
 
-                rowElement.appendChild(cellElement);
-            }
-            boardElement.appendChild(rowElement);
-        }
-    }; 
+  playerDisplay.textContent = `${currentPlayer}'s turn`;
+};
 
-    function addLetter (evt) {
-        let targetedElement = evt.target;
-        let currentPlayer = gameState.currentPlayer;
-        if (currentPlayer === 'X') {
-            targetedElement.innerText = 'X';
-            gameState.currentPlayer = 'O';       
-        } else if (currentPlayer === 'O') {
-            targetedElement.innerText = 'O';
-            gameState.currentPlayer = 'X';
-        }
-    };
-    
-    let resetButton = document.getElementById('resetButton');
-    resetButton.addEventListener('click', resetBoard);
-    function resetBoard() {  
-        gameState = {
-            gameBoard: [
-                [null, null, null],
-                [null, null, null],
-                [null, null, null]
-            ],
-            currentPlayer: 'X',
-            players: ['X', 'O']
-        };
-        boardElement.innerText = '';
-        createGameBoard();
+const announce = (type) => {
+  switch (type) {
+    case playerOWon:
+      announcer.innerHTML = `${currentPlayer} has won`;
+      break;
+    case playerXWon:
+      announcer.innerHTML = `${currentPlayer} has won`;
+      break;
+    case draw:
+      announcer.innerText = "Draw";
+  }
+  announcer.classList.toggle("hide");
+};
+
+function handleResultValidation() {
+  let roundWon = false;
+  for (let i = 0; i <= 7; i++) {
+    const winCondition = winningConditions[i];
+    const a = board[winCondition[0]];
+    const b = board[winCondition[1]];
+    const c = board[winCondition[2]];
+    if (a === "" || b === "" || c === "") {
+      continue;
     }
+    if (a === b && b === c) {
+      roundWon = true;
+      break;
+    }
+  }
+  if (roundWon) {
+    announce(isX === "X" ? playerXWon : playerOWon);
+    isGameActive = false;
+    return;
+  }
+  if (!board.includes("")) announce(draw);
+}
 
-    
+const userAction = (tile, index) => {
+  if (isValidAction(tile) && isGameActive) {
+    tile.innerText = isX;
+    tile.classList.add(`player${isX}`);
+    updateBoard(index);
+    handleResultValidation();
+    changePlayer();
+  }
+};
 
+tiles.forEach((tile, index) => {
+  tile.addEventListener(`click`, () => userAction(tile, index));
+});
 
+const resetBoard = () => {
+  board = ["", "", "", "", "", "", "", "", ""];
+  isGameActive = true;
+  announcer.innerHTML = "";
+  
+  if (isX === "O") {
+    changePlayer();
+  }
+  
+  tiles.forEach((tile) => {
+    tile.innerText = "";
+    tile.classList.remove("playerX");
+    tile.classList.remove("playerO");
+  });
+  
+};
 
-})
-
-// Problem #2: Getting your cells to do SOMETHING
-// Step 1) Grab the part of your website you awnt to do something
-    // I did that in my nested for loop above where I create a td element. 
-// Step 2) What do I wnat to teach the cells to do?
-    // I need to write a callback function 
-// Step 3) How do we connect these two steps together?
+resetButton.addEventListener("click", resetBoard);
